7c08b9e35a74e9a59e8a64229f12a8add60a9f15
Anatoly Yakovenko <aeyakovenko@gmail.com>
2018-02-05 15:07:07 -0800

    wip

--- src/reader.rs
+++ src/reader.rs
@@ -1,6 +1,7 @@
 use std::sync::{Arc, Mutex};
 use std::collections::VecDeque;
 use std::net::{SocketAddr, Ipv4Addr, IpAddr};
+use result::{Result, from_option};
 use mio;
 use data;
 use net;
@@ -16,21 +17,22 @@ struct Reader {
     port: u16,
 }
 impl Reader {
-    pub fn new(port: u16) -> Result<Reader> {
+    pub fn new(port: u16) -> Reader {
         let d = Data { gc: Vec::new(),
-                       pending: Vec::new() };
+                       pending: VecDeque::new() };
 
         return Reader{lock: Mutex::new(d), port: port};
     }
     pub fn next(&self) -> Result<Messages> {
-        let d = self.lock.lock();
-        d.pending.pop_front()
+        let d = self.lock.lock().expect("lock");
+        let o = d.pending.pop_front();
+        return from_option(o);
     }
-    pub fn recycle(&self, m: Messages) -> Result<Messages> {
-        let d = self.lock.lock();
-        d.gc.push(m)
+    pub fn recycle(&self, m: Messages) {
+        let d = self.lock.lock().expect("lock");
+        d.gc.push(m);
     }
-    pub fn run(&self) {
+    pub fn run(&self) -> Result<()> {
         let mut num = 0usize;
         let ipv4 = Ipv4Addr::new(0, 0, 0, 0);
         let addr = SocketAddr::new(IpAddr::V4(ipv4), self.port);
@@ -48,13 +50,14 @@ impl Reader {
             self.enqueue(m);
             self.notify();
         }
+        return Ok(());
     }
     fn notify(&self) {
         //TODO(anatoly), hard code other threads to notify
     }
     fn allocate(&self) -> Messages {
-        let d = self.lock.lock();
-        return match d.gc.pop() {
+        let s = self.lock.lock();
+        return match s.gc.pop() {
                 Some(v) => {
                     v.2 = 0;
                     v;
@@ -64,14 +67,14 @@ impl Reader {
                     m.resize(1024, data::Message::default());
                     let mut d = Vec::new();
                     d.resize(1024, Default::default());
-                    Arc::new((v, d, 0))
+                    Arc::new((m, d, 0))
                 }
         }
     }
     fn enqueue(&self, m: Messages) {
-        let d = self.lock.lock();
-        d.pending.push_back(m);
-        return match d.gc.pop() {
+        let s = self.lock.lock();
+        s.pending.push_back(m);
+        return match s.gc.pop() {
                 Some(v) => v,
                 _ => {
                     let mut v = Vec::new();
